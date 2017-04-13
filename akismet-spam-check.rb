41e244d93ae5d5ba1a6f3044142676fb2f05cf0a
Cthulhu <Cthulhu@JAD-Macbook-Pro.local>
2017-04-12 18:59:19 -0700

    add akismet spam check

---
+++ akismet-spam-check.rb
@@ -0,0 +1,39 @@
+		
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require 'httparty'
+
+
+
+filename = ARGV[0].to_s
+puts "Parsing file: #{filename}"
+
+#proxy_addr = "127.0.0.1"
+#proxy_port = 8080
+
+file = File.open("#{filename}", "r")
+
+file.each do |line|
+	comment = line
+	uri = URI.parse("http://rest.akismet.com/1.1/comment-check")
+	http = Net::HTTP.new(uri.host, uri.port)
+	#request = Net::HTTP::Post.new(uri.request_uri)
+	begin
+		#request.set_form_data({"comment_content" => "#{comment}", "user_ip" => "", 
+		#"comment_type" => "comment", "blog" => "https://bacon","is_test" => "1"}) response 
+		#= HTTParty.post('http://rest.akismet.com/1.1/comment-check')
+    response = HTTParty.post("http://rest.akismet.com/1.1/comment-check",
+    :query => { "comment_content" => "#{comment}", "user_ip" => "", "comment_type" => "comment", 
+    :"blog" => "https://bacon","is_test" => "1" })
+    puts response
+
+	rescue StandardError=>e
+	   puts "\tError: #{e}"
+	end
+	#response = http.request(request)
+	puts "#{response}"
+
+end
+
+
